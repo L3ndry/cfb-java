4dc11b897650a1c4b669bea82182932338c02f09
Wagner Leandro <leandro.login@hotmail.com>
2021-11-10 14:18:53 -0300

    Created cursojava12.java, jogador12.java

---
+++ cursojava12.java
@@ -0,0 +1,7 @@
+public class cursojava12 {
+    public static void main(String[] args) {
+        jogador12 jogador1 = new jogador12(1);
+        jogador12 jogador2 = new jogador12(2);
+        jogador12 jogador3 = new jogador12(3);
+    }
+}
--- jogador12.java
+++ jogador12.java
@@ -1,11 +1,14 @@
-public class jogador11 {
-    
+public class jogador12 {
+
     private int numero=0;
     private int vidas=0;
-    public jogador11(int num){
+
+    public jogador12(int num) {
         this.numero=num;
         this.vidas=3;
         System.out.printf("Jogador número %d criado!%n", num);
-        System.out.printf("O jogador número %d possui %d vidas!", num, vidas);
+        System.out.printf("O jogador número %d possui %d vidas!%n", num, vidas);
     }
 }
+
+

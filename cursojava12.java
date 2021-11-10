7d026ea03c5023db7229cefb4087736e437dcc93
Wagner Leandro <leandro.login@hotmail.com>
2021-11-10 15:38:05 -0300

    Created cursojava12.java, jogador12.java

--- cursojava12.java
+++ cursojava12.java
@@ -1,7 +1,20 @@
 public class cursojava12 {
     public static void main(String[] args) {
-        jogador12 jogador1 = new jogador12(1);
-        jogador12 jogador2 = new jogador12(2);
-        jogador12 jogador3 = new jogador12(3);
+
+        int num=0;
+
+        jogador12 jogador1 = new jogador12(num++);
+        jogador12 jogador2 = new jogador12(num++);
+        jogador12 jogador3 = new jogador12(num++);
+
+        jogador3.addVidas();
+        jogador3.addVidas();
+        jogador3.addVidas();
+        jogador3.addVidas();
+        jogador1.addVidas();
+
+        System.out.printf("O jogador 1 possui %d vida(s)!%n", jogador1.getVidas());
+        System.out.printf("O jogador 2 possui %d vida(s)!%n", jogador2.getVidas());
+        System.out.printf("O jogador 3 possui %d vida(s)!", jogador3.getVidas());
     }
 }
--- jogador12.java
+++ jogador12.java
@@ -1,13 +1,23 @@
 public class jogador12 {
 
+    private final int maxVidas=3;
     private int numero=0;
     private int vidas=0;
 
     public jogador12(int num) {
         this.numero=num;
-        this.vidas=3;
+        this.vidas=1;
         System.out.printf("Jogador número %d criado!%n", num);
-        System.out.printf("O jogador número %d possui %d vidas!%n", num, vidas);
+    }
+
+    public int getVidas(){
+        return this.vidas;
+    }
+
+    public void addVidas(){
+        if(this.vidas < maxVidas){
+            this.vidas++;
+        }
     }
 }
 
